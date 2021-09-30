009ae8632307f12a0750556e1559dc4438a59d97
Yuval14498 <88030185+Yuval14498@users.noreply.github.com>
2021-09-30 22:49:30 +0300

    removed controlHeight.js

--- utils/controlHeight.js
+++
@@ -1,7 +0,0 @@
-const actualHeight = window.innerHeight;
-const elementHeight = document.getElementById('control-height').clientHeight;
-const barHeight = elementHeight - actualHeight;
-const index = document.getElementById('index')
-const bgImage = document.getElementById('background-image')
-index.style.height = index.clientHeight - barHeight
-bgImage.style.height = bgImage.clientHeight - barHeight
